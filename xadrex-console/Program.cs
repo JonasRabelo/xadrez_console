ee7ab73505b1e9d7b8ad8bd83df3489e453dc1a1
JonasRabelo <jonas2mr01@gmail.com>
2023-09-04 13:56:22 -0300

    Começando a implementar o tabuleiro

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -7,9 +7,9 @@ namespace xadrez_console {
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(2, 3);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine("posição: " + P);
+            Console.WriteLine();
         }
     }
 }
---
+++ xadrex-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linhas { get; set; }
+        public int Colunas { get; set;}
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            Linhas = linha;
+            Colunas = coluna;
+            pecas = new Peca[Linhas, Colunas];
+        }
+    }
+}
