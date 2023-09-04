194d6fbaf21ba77edd8f4f587c3f1a3998d60716
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-09-04 18:01:40 -0300

    Começando a colocar peças no tabuleiro

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using tabuleiro;
+using xadrez;
 using xadrex_console;
 
 namespace xadrez_console { 
@@ -10,6 +11,9 @@ namespace xadrez_console {
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
             Tela.ImprimirTabuleiro(tab);
             Console.WriteLine();
 
