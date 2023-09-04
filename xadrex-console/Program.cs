1249f5a3b5db9004a8d3040b82e9dc8a59265c15
JonasRabelo <jonas2mr01@gmail.com>
2023-09-04 13:38:45 -0300

    Projeto Criado

---
+++ xadrex-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace xadrez_console { 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
