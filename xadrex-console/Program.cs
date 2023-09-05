a191b1d2030f3348012c6314c2a8298c6c1250a6
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-09-04 21:42:33 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -4,19 +4,34 @@ using tabuleiro;
 using xadrez;
 using xadrex_console;
 
-namespace xadrez_console { 
+namespace xadrez_console
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.tab);
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            tab.ColocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 6));
-
-            Tela.ImprimirTabuleiro(tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                
+                    partida.ExecutarMovimento(origem, destino);
+                }
+                
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.WriteLine();
 
 
---
+++ xadrex-console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PartidaDeXadrez
+    {
+        public Tabuleiro tab { get; private set; }
+        private int turno;
+        private Cor jogadorAtual;
+        public bool terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            tab = new Tabuleiro(8, 8);
+            turno = 1;
+            jogadorAtual = Cor.Branca;
+            terminada = false;
+            ColocarPecas();
+        }
+
+        public void ExecutarMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = tab.RetirarPeca(origem);
+            p.IncrementarQteMovimentos();
+            Peca pecaCapturada = tab.RetirarPeca(destino);
+            tab.ColocarPeca(p, destino);
+        }
+        
+        public void ColocarPecas()
+        {
+            tab.ColocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadrez('c',1).ToPosicao());
+            
+        }
+
+    }
+}
