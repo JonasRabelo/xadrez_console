d585a7208086eeeaed6567a2d7482a0d9c698625
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-09-06 16:56:44 -0300

    Testes iniciais com movimentos possiveis das pecas

--- xadrex-console/Program.cs
+++ xadrex-console/Program.cs
@@ -21,6 +21,11 @@ namespace xadrez_console
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                    
+                    bool[,] PosicoesPossiveis = partida.tab.Peca(origem).MovimentosPossiveis();
+                    Console.Clear();
+                    Tela.ImprimirTabuleiro(partida.tab, PosicoesPossiveis);
+
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
                 
